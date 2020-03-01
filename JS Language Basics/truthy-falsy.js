aebc74b8ccf35c70049d7be09a138cdfc85c69f1
Hady Salhab <hadysalhab1@gmail.com>
2020-03-02 10:47:59 +1100

    truthy falsy and equality operator

---
+++ JS Language Basics/truthy-falsy.js
@@ -0,0 +1,19 @@
+// falsy values: undefined , null , 0 , '' , NaN;
+//truthy values: All values that are not falsy.
+
+var height;
+height = 23;
+height = 0; //this is a falsy value
+//work around, to avoid printing value is not defined when its actually defined with value 0
+if (height || height === 0) {
+  console.log("Variable is defined");
+} else {
+  console.log("Variable has not been defined");
+}
+
+// ==  type coersion, they are equal even if their type does not match
+// === their value and type must be equal
+
+if (height == "23") {
+  console.log("the == operator does type coersion");
+}
